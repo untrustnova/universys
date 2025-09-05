adc4a2938fcd7cb515964e09e61a5873190d92de
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2025-09-05 21:11:13 +0700

    change: ui

--- src/content/Herosection.jsx
+++ src/content/Herosection.jsx
@@ -4,22 +4,25 @@ export default function Herosection({ searchText, setSearchText }) {
       className="w-full h-full object-cover"
       src="./arisu-yuzu-wp.jpg"
     />
-    <div className="absolute w-full h-full flex items-center justify-center bgimage-noise-medium backdrop-blur-[2.5px] bg-black/40 px-4">
-      <div className="w-full max-w-2xs absolute flex justify-end mt-[-95px]">
+    <div className="absolute w-full h-full flex flex-col items-center justify-center bgimage-noise-medium backdrop-blur-[2.5px] bg-black/55 px-4">
+      <div className="w-full max-w-2xs flex items-start">
         <img
           src="./arisu.webp"
-          className="w-[75px]"
+          className="w-[75px] -mt-[55px] absolute"
+        />
+        <input
+          name="search-q"
+          id="search-q"
+          className="w-full max-w-2xs px-3.5 py-2 text-center font-jetbrain font-semibold outline-none bg-neutral-800/50 text-white pixel-corners bgimage-noise-state"
+          autoComplete="off"
+          placeholder="Search command in here!"
+          value={searchText}
+          onChange={(e) => { setSearchText(String(e.target?.value||"")) }}
         />
       </div>
-      <input
-        name="search-q"
-        id="search-q"
-        className="w-full max-w-2xs px-3.5 py-2 text-center font-jetbrain font-semibold outline-none bg-neutral-800/50 text-white pixel-corners bgimage-noise-state"
-        autoComplete="off"
-        placeholder="Search command in here!"
-        value={searchText}
-        onChange={(e) => { setSearchText(String(e.target?.value||"")) }}
-      />
+      <div className="w-full max-w-2xs pt-2.5">
+        <p className="text-center text-white">Search any command in here sensei!</p>
+      </div>
     </div>
   </div>
 }
--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,20 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
-const isBaseOnPages = !!(process.argv.includes("--basehomepages"))
-let basePath = "/"
-if(isBaseOnPages) {
-  const requirepackage = require("./package.json")
-  const parseURL = new URL(requirepackage.homepage)
-  basePath = parseURL.pathname
-}
+// const isBaseOnPages = !!(process.argv.includes("--basehomepages"))
+// let basePath = "/"
+// if(isBaseOnPages) {
+//   const requirepackage = require("./package.json")
+//   const parseURL = new URL(requirepackage.homepage)
+//   basePath = parseURL.pathname
+// }
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
-  base: basePath,
+  base: "/",
   server: {
     host: "0.0.0.0"
   }
